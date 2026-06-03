b1d5203d1d41ac5517f9a4c5ef5b2c2826ba89f8
thiha129 <thim00549@gmail.com>
2026-06-03 22:13:32 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL =
-  "https://backend-booking-tour-dulich.onrender.com/api/v1/" ||
+  "https://backend-booking-tour-dulich.onrender.com/api/v1" ||
   "http://localhost:4000/api/v1";
